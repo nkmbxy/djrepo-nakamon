09d77cf33bebed15a886d778c687c0fa12baddd0
nkmbxy <nakamon22547@gmail.com>
2026-03-12 15:58:48 +0700

    add superuser creation step in build script

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+  python manage.py createsuperuser \
+    --no-input \
+    --username $DJANGO_SUPERUSER_USERNAME \
+    --email $DJANGO_SUPERUSER_EMAIL
+fi
